d38046ca0480b2d92e2a9bb9f49853afd93b4b41
Charles <charles@Charless-Mac-mini.local>
2024-09-29 21:21:56 +0530

    initial commit

---
+++ ImageListApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageListApp
+//
+//  Created by Charles on 29/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageListApp/ImageListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageListAppApp.swift
+//  ImageListApp
+//
+//  Created by Charles on 29/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
